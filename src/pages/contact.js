6d49eac24e2124ff8d7ec48b0882b51bd7a7987c
BuffManKyle <epsolfwd@gmail.com>
2022-12-01 14:21:04 -0500

    env variable not available for email.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -64,7 +64,7 @@ function Contact() {
                                     </Col>
                                   
                                     <Button type="Submit" value="send" className="Contact-button">
-                                        Send
+                                        Send 
                                     </Button>
                             </form>
                             <ToastContainer/>
