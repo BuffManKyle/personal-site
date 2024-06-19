5840bb0771c49c4dd4e3614f16d735bd330f6077
BuffManKyle <epsolfwd@gmail.com>
2024-06-19 11:26:04 -0400

    This one will work right?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './pages/styles.css';
+import React from "react";
 import { Route, Routes } from 'react-router-dom'
 import Blog from './pages/blog'
 import Home from './pages/home';
