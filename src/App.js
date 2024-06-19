1b43c0c28258efb043d2cf30112acc576eafa7bd
BuffManKyle <epsolfwd@gmail.com>
2024-06-19 11:11:52 -0400

    This will work, restructure

--- src/App.js
+++ src/App.js
@@ -1,63 +1,19 @@
 import './App.css';
 import './pages/styles.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Slide from 'react-reveal/Slide';
+import Blog from './pages/blog'
 import Home from './pages/home';
-import Portfolio from './pages/portfolio';
-import About from './pages/about';
-import Skills from './pages/skills';
-import Contact from './pages/contact';
-import Blog from './pages/blog';
-import Header from './pages/partials/header';
-import Footer from './pages/partials/footer';
-import {Nav} from 'react-bootstrap';
-
-function MainPage() {
-  return (
-    <div className='Body-css'>
-      <Header />
-      
-      <div className='sections' id="home"><Home /></div>
-
-      <Slide left>
-        <div className='sections' id="portfolio"><Portfolio /></div>
-      </Slide>
-
-      <Slide right>
-        <div className='sections' id="about"><About /></div>
-      </Slide>
-
-      <Slide left>
-        <div className='sections' id="skills"><Skills /></div>
-      </Slide>
-
-      <Slide right>
-        <div className='sections' id="contact"><Contact /></div>
-      </Slide>
-      <Nav.Link href="/blog" >Home</Nav.Link>
-        <Footer />
-    </div>
-  );
-}
-
-function BlogPage() {
-  return (
-    <div className='Body-css'>
-      
-      <div className='sections' id="blog"><Blog /></div>
-      
-    </div>
-  );
-}
 
 function App() {
   return (
+    <div className='Body-css'>
     <Router>
       <Routes>
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/" element={<MainPage />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
+    </div>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from 'react-bootstrap';
 import './styles.css'
 import React from "react";
-
+import Portfolio from '../pages/portfolio';
+import About from '../pages/about';
+import Skills from '../pages/skills';
+import Contact from '../pages/contact';
+import Header from '../pages/partials/header';
+import Footer from '../pages/partials/footer';
+import {Nav} from 'react-bootstrap';
+import Slide from 'react-reveal/Slide';
 
 class TypingEffect extends React.Component {
     state = {
@@ -55,6 +62,8 @@ class TypingEffect extends React.Component {
   }
 function Home() {
     return (
+                <div>
+                  <Header />
                 <Container fluid>
                     <div className="backlight-section">
                         <Container >
@@ -76,8 +85,28 @@ function Home() {
                             </Row>
                         </Container>
                     </div>
+                    <div className='Body-css'>
+                      <Slide left>
+                        <div className='sections' id="portfolio"><Portfolio /></div>
+                      </Slide>
+
+                      <Slide right>
+                        <div className='sections' id="about"><About /></div>
+                      </Slide>
+
+                      <Slide left>
+                        <div className='sections' id="skills"><Skills /></div>
+                      </Slide>
+
+                      <Slide right>
+                        <div className='sections' id="contact"><Contact /></div>
+                      </Slide>
+                      <Nav.Link href="/blog" >Test Link</Nav.Link>
+                        <Footer />
+                    </div>
                 </Container>
-                
+              
+                </div>
     );
 }
 
